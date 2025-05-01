c196a171714362000a74ca7eabb71f98de1df244
Prabhat-kumaar <prabhatyadav.dbg@gmail.com>
2025-05-01 11:22:17 +0530

    First program of java with baisc explanation

---
+++ Basic-of-java/Helloworld.java
@@ -0,0 +1,30 @@
+public class Helloworld{
+    public static void main(String[] args) {
+        System.out.println("Hello world This java");
+    }
+}
+
+/*Explations 
+ * 
+ * public-> public is a access modifier. means it class is accessible from anywhere in your java progam and project
+ * class-> A java keyword used to define a class(it's like a blue print for objects) 
+ * HelloWorld->it's a class name in java. class name is usually start witha capital letter ---This style is called CamelCase.
+ * 
+ * Here are a few simple examples of class names in CamelCase:
+
+    1.StudentDetails
+
+    2.BankAccount
+
+    3.CarEngine
+
+    4.BookStore
+
+    5.MobilePhone
+
+    Each word starts with a capital letter, and there are no spaces between words.
+ * 
+ * 
+ * 
+ * 
+*/
