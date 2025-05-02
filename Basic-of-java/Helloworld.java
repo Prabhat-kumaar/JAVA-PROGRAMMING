bc633c92900545d5ebe6b2565bfca256bc76e3c0
Prabhat-kumaar <prabhatyadav.dbg@gmail.com>
2025-05-02 11:21:17 +0530

    Nothing changed on this code

--- Basic-of-java/Helloworld.java
+++ Basic-of-java/Helloworld.java
@@ -1,14 +1,14 @@
-public class Helloworld{
+public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world This java");
     }
 }
 
-/*Explations 
+/*Explanations 
  * 
- * public-> public is a access modifier. means it class is accessible from anywhere in your java progam and project
+ * public-> public is a access modifier. means it class is accessible from anywhere in your java program and project
  * class-> A java keyword used to define a class(it's like a blue print for objects) 
- * HelloWorld->it's a class name in java. class name is usually start witha capital letter ---This style is called CamelCase.
+ * HelloWorld->it's a class name in java. class name is usually start with a capital letter ---This style is called CamelCase.
  * 
  * Here are a few simple examples of class names in CamelCase:
 
