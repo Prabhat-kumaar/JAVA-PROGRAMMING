851e13027d528ffd4acb2b11220a0b1b6e34f22d
Prabhat-kumaar <prabhatyadav.dbg@gmail.com>
2025-05-02 12:05:20 +0530

    just learn How Relationaloperator are working

---
+++ Basic-of-java/Relational_Operator.java
@@ -0,0 +1,29 @@
+/*Relational Operator is also known as  comparison Operator 
+ * 
+ *      it is used to compare values:
+ * 
+*/
+public class Relational_Operator {
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("num1 == num2:" + (num1 == num2));  //equal to operator
+        System.out.println("num1 =! num2:" + (num1 != num2));  //not equal to operator
+        System.out.println("num1 > num2:" + (num1 > num2));  //Grater than operator
+        System.out.println("num1 < num2:" + (num1 < num2));  //Less than
+        System.out.println("num1 >= num2:" + (num1 >= num2)); //Greater than and equal to operator
+        System.out.println("num1 <= num2:" + (num1 <= num2)); //Less than and equal to operator
+
+        
+        // This is first program of if condition just for check it is working or not
+       int num =10;
+        if (num == 10){
+            System.out.println("Hello");
+        }
+            
+
+    }
+    
+}
