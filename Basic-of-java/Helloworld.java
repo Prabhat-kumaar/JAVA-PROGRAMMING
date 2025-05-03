02aeca27f9a1edb8690f0bd6889aefc4ec319b71
Prabhat-kumaar <prabhatyadav.dbg@gmail.com>
2025-05-03 23:18:06 +0530

    I just explaing to friend

--- Basic-of-java/Helloworld.java
+++ Basic-of-java/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world This java");
+        System.out.println("Hello world my name is prabhat");
     }
 }
 
